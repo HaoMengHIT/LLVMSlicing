d3e94565463ec0a6522e972519cde3ff9a76dbcd
HaoMengHIT <haomeng@hit.edu.cn>
2018-05-25 00:37:37 +1400

    add test

---
+++ test/sum_readfile.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <omp.h>
+#include <math.h>
+#include <stdlib.h>
+
+int sum = 0;
+
+void getsum(int start, int end)
+{
+   for(int i = start; i < end;i++)
+   {
+      sum+=sqrt(i);
+   }
+   return;
+
+}
+void readFile(int& start, int& end)
+{
+   FILE *fp;
+   fp = fopen("num.txt","r");
+   fscanf(fp,"%d %d",&start, &end);
+   printf("start = %d, end = %d\n",start, end);
+   fclose(fp);
+}
+int main()
+{
+   int start, end;
+   readFile(start, end);
+   printf("Init sum = %d\n",sum);
+   getsum(start, end);
+   printf("Final sum = %d\n",sum);
+   return 0;
+}
